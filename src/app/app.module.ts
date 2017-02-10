b1b36aef89738ea9e517a458738de49a151f7b6d
Brook Hutchinson <brookhutchinson@outlook.com>
2017-02-10 12:28:04 -0600

    Change package.json "version" property value from "0.0.0" to "1.0.0", Change package.json devDependencies "typescript" property value from "~2.0.3" to "2.1.5", Remove whitespace, Format files, Create new folders under "src/app", Move "styles.css" from folder "src" to folder "src/app/css", Change "styles" property value file path to "app/css/styles.css" inside angular-cli.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
+// modules
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
+// components
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
+  // modules
+  imports: [ BrowserModule, HttpModule, FormsModule ],
+  // components
+  declarations: [ AppComponent ],
+  // services
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+// this file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/proxy.js';
@@ -6,27 +6,24 @@ import 'zone.js/dist/sync-test';
 import 'zone.js/dist/jasmine-patch';
 import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
-import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
+import { getTestBed }                                                 from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
-// Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
+// there is no typing for the `__karma__` variable, declare variable as any
 declare var __karma__: any;
 declare var require: any;
 
-// Prevent Karma from running prematurely.
+// prevent karma from running prematurely
 __karma__.loaded = function () {};
 
-// First, initialize the Angular testing environment.
+// initialize the Angular testing environment
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 );
-// Then we find all the tests.
+// find all the tests
 const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
+// load the modules
 context.keys().map(context);
-// Finally, start Karma to run the tests.
+// start karma to run the tests
 __karma__.start();
