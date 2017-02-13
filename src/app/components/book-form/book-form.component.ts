37566bdd2fd652cefb6e4c315ad02b80a8f88180
Brook Hutchinson <brookhutchinson@outlook.com>
2017-02-13 17:22:12 -0600

    Use "Book" Model inside TypeScript BookFormComponent Class, Write TypeScript inside book-form.component.ts, Import Book Model, Define a property named "book" of type Book, Create new "book" object by initializing the "book" property values using the "new" keyword inside the constructor() function

--- src/app/components/book-form/book-form.component.ts
+++ src/app/components/book-form/book-form.component.ts
@@ -1,8 +1,20 @@
+// components
 import { Component } from '@angular/core';
+
+// models
+import { Book }      from '../../models/book.model';
 
 @Component({
   selector: 'book-form',
   templateUrl: './book-form.component.html',
   styleUrls: ['./book-form.component.css']
 })
-export class BookFormComponent {}
+export class BookFormComponent {
+  // define properties
+  private book: Book;
+
+  constructor() {
+    // create book object
+    this.book = new Book(1, '', '', 'http://');
+  }
+}
